296fafd514d910af534b9dab168de9183de69e1c
Timo Kallaste <timo.kallaste@mobisolutions.com>
2013-03-21 21:07:36 +0200

    Initial commit to set up project

---
+++ FogOfFear/FogOfFear/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  FogOfFearAppController.h
+//  FogOfFear
+//
+//  Created by Timo Kallaste on 3/21/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
